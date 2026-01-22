7146a817d9e1aeebfaa0c35eee5a9244e489cfa4
g00097537-oss <g00097537@aus.edu>
2026-01-22 19:59:28 +0400

    Feature B added

--- Hello.java
+++ Hello.java
@@ -2,4 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+    
+    public static void add(int a, int b) {
+    	System.out.println(a + b);
+    }
 }
